90cde3fa04f952aa7939350db5474f6ce7d58ca9
Kumari Ranjana Yadav <yranjana757@gmail.com>
2024-05-07 15:24:02 +0530

    improved UI

--- client/src/Component/Footer.jsx
+++ client/src/Component/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='bg-gray-500 w-full text-white py-6 px-0 flex flex-col mt-12 justify-center items-center '>
+    <div className='bg-black w-full text-white py-6 px-0 flex flex-col mt-12 justify-center items-center '>
       <p>Created by Ranjana </p>
       <p>All right reserved 2024 </p>
     </div>
