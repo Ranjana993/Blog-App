2bda9575e23bbacbaf916e2d6fed7d4fe21e679c
Kumari Ranjana Yadav <yranjana757@gmail.com>
2024-05-08 22:22:07 +0530

    added footer

--- client/src/Component/Footer.jsx
+++ client/src/Component/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className='bg-black w-full text-white py-6 px-0 flex flex-col mt-12 justify-center items-center '>
       <p>Created by Ranjana </p>
-      <p>All right reserved 2024 </p>
+      <p>All right reserved 2024 </p> 
     </div>
   )
 }
